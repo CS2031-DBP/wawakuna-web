da48c41a77a0809d8d0c6f554e917f5a15be990d
m41k12 <michael.hinojosa@utec.edu.pe>
2024-07-08 00:51:12 -0500

    a

--- control_parental/src/utils/Utils.jsx
+++ control_parental/src/utils/Utils.jsx
@@ -8,7 +8,7 @@ export const initializeState = (keys) => {
     return obj;
 };
 
-export const API_URL = "http://localhost:8080";
+export const API_URL = "http://54.198.164.202:8080";
 
 export const getToken = () => {
     return localStorage.getItem('token');
